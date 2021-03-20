0681ce413566ec77987cb8eeb4f757eb8ad152e3
zionjke <vincere802@gmail.com>
2021-03-20 14:26:10 +0200

    refactoring all components, add new api,add new icons ++

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,14 @@ function App() {
 
 
     useEffect(() => {
-        axios.get(`https://aviation-edge.com/v2/public/timetable?key=${API_KEY}=KBP&type=arrival`)
+        axios.get(`https://aviation-edge.com/v2/public/timetable?key=${API_KEY}=KBP&type=arrival&lang=ru`)
             .then(({data}) => setState(data))
     }, [])
 
     console.log(state)
     // @ts-ignore
     const filteredState = state.filter(item => item.arrival.scheduledTime >= now)
-    console.log(filteredState.slice(0,26))
+    console.log(filteredState)
     console.log(now)
 
     return (
