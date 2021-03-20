9f45fd790e69250808ea36933a36b282432cd992
zionjke <vincere802@gmail.com>
2021-03-20 13:46:12 +0200

    refactoring all components, add new api,add new icons +

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,31 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Schedule} from "./components/Schedule/Schedule";
 import axios from "axios";
+import moment from "moment";
 
 
 function App() {
 
-    const [state,setState] = useState([])
+    const [state, setState] = useState([])
     const API_KEY = '3fe0e8-91926e&iataCode'
+    let now = moment().format()
+
 
     useEffect(() => {
-        axios.get(`https://aviation-edge.com/v2/public/timetable?key=${API_KEY}=KBP&type=arrival&arr_terminal=D&status=active`)
-            .then(({data}) => setState(data) )
-    },[])
+        axios.get(`https://aviation-edge.com/v2/public/timetable?key=${API_KEY}=KBP&type=arrival`)
+            .then(({data}) => setState(data))
+    }, [])
 
     console.log(state)
+    // @ts-ignore
+    const filteredState = state.filter(item => item.arrival.scheduledTime >= now)
+    console.log(filteredState.slice(0,26))
+    console.log(now)
 
     return (
         <div className="App">
             <Header/>
-            <Schedule />
+            <Schedule/>
         </div>
     );
 }
