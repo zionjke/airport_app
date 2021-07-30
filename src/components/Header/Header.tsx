84293f6dca0c73e1463de3ae0398353279040634
artem_vasiliev <vincere802@gmail.com>
2021-07-30 10:32:17 +0300

    landing update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import kloIcon from '../../assets/kloIcon.png'
+import kloIcon2 from '../../assets/kloIcon2.png'
 import airPlaneIcon from '../../assets/source.gif'
 import style from './Header.module.css'
 import {Weather} from "../Weather/Weather";
@@ -11,8 +11,8 @@ export const Header: React.FC<HeaderProps> = () => {
     return (
         <div className={style.header}>
             <div className={style.icons}>
-                <img className={style.airplaneIcon} src={airPlaneIcon} alt=""/>
-                <img className={style.headerIcon} src={kloIcon} alt="Klo Icon"/>
+                {/*<img className={style.airplaneIcon} src={airPlaneIcon} alt=""/>*/}
+                <img className={''} src={kloIcon2} alt="Klo Icon"/>
             </div>
             <Weather/>
         </div>
